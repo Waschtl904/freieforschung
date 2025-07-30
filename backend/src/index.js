1cf06065214cffe2d534489f558b5f3719a4eb10
Waschtl904 <sebastian.schmalnauer@gmx.at>
2025-07-30 19:23:44 +0200

    Backend-Grundgerüst mit Express

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+﻿require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+
+app.get('/api/health', (req, res) => {
+  res.json({ status: 'OK' });
+});
+
+app.listen(port, () => {
+  console.log(http://localhost:4200);
+});
